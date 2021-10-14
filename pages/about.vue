40fe4371769ceff2caa24b0f1aff086fd456661f
OlivierC <coq.olivier36@gmail.com>
2021-10-13 22:32:57 -0400

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>About Dad Jokes</h1>
+      <p>Displays terribe dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head(){
+        return {
+            title: 'About The Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
